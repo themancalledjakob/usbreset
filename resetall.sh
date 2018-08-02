228f93a5635d8c031bbb78470eac329bbbf4c501
themancalledjakob <j.schloetter@gmail.com>
2018-08-02 07:41:11 +0200

    more methods of reset

--- resetall.sh
+++ resetall.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+pass='pp'
+if [ $# -eq 1 ]
+  then
+    pass=$1
+fi
 ls /dev/bus/usb
 for f in /dev/bus/usb/*; do
     bus=$(basename $f)
@@ -6,7 +11,29 @@ for f in /dev/bus/usb/*; do
         port=$(basename $p)
         udevadm info --query=all --attribute-walk --name=$p | grep 'RealSense' &> /dev/null
         if [ $? == 0 ]; then
-            echo pp | sudo -S ./usbreset $p
+            echo $pass | sudo -S ./usbreset $p
         fi
     done
 done
+for f in /dev/bus/usb/*; do
+    bus=$(basename $f)
+    for p in ${f}/*; do
+        port=$(basename $p)
+        udevadm info --query=all --attribute-walk --name=$p | grep 'RealSense' &> /dev/null
+        if [ $? == 0 ]; then
+            echo $pass | sudo -S usb_modeswitch -v 0x8086 -p 0x0ad3 -b $bus -g $port -R
+        fi
+    done
+done
+for f in /dev/bus/usb/*; do
+    bus=$(basename $f)
+    for p in ${f}/*; do
+        port=$(basename $p)
+        udevadm info --query=all --attribute-walk --name=$p | grep 'RealSense' &> /dev/null
+        if [ $? == 0 ]; then
+            echo $pass | sudo -S usb_modeswitch -v 0x8086 -p 0x0ad3 -b $bus -g $port -d
+        fi
+    done
+done
+sudo rmmod uvcvideo
+sudo modprobe uvcvideo
